4afd19a90271c39cd76f918d4a3a826f3ab4a0eb
mutedalien <chel_c@mail.ru>
2020-04-11 19:57:40 +0300

    Урок 4 Встроенные типы и операции с ними

--- less_4/less4-1.py
+++ less_4/less4-1.py
@@ -1,6 +1,9 @@
+# Даны два произвольные списка. Удалите из первого списка элементы присутствующие во втором списке.
+
 my_list_1 = [2, 5, 8, 2, 2, 2, 2, 12, 12, 4]
 my_list_2 = [2, 7, 12, 3]
 for number in my_list_2:
-    while number in my_list_1:
+    while number in my_list_1[:]: # [:] делаем срез от первого элемента до последнего
+        if number in my_list_2:
         my_list_1.remove(number)
 print(my_list_1)

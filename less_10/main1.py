a6270af93e72f3e49e858a45ce24f955eb5ef926
mutedalien <chel_c@mail.ru>
2020-04-14 09:19:47 +0300

    Урок 10 Модули и библиотеки

---
+++ less_10/main1.py
@@ -0,0 +1,6 @@
+import math
+import random as rd
+
+print(math.pi) # Вернет число Пи: 3.141592653589793
+print(math.sin(38)) # Вернет синус угла 38гр: 0.2963685787093853
+print(rd.randint(1, 10)) # Вернет рандомное значение от 1 до 10

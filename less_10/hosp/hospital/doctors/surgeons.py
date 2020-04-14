d61b0d124806cd95a4834e0a85d014fd1a177f2c
mutedalien <chel_c@mail.ru>
2020-04-14 15:42:33 +0300

    Урок 10 Модули и библиотеки

---
+++ less_10/hosp/hospital/doctors/surgeons.py
@@ -0,0 +1,5 @@
+from .murses import get_murses
+
+def get_surgeons():
+    get_murses()
+    print('Хирурги из пакета доктора')
---
+++ less_10/hosp/hospital/h.py
@@ -0,0 +1,5 @@
+from  .doctors.surgeons import get_surgeons
+
+def get_main():
+    print('Med')
+    get_surgeons()

bdbb903712551bc3c990d7bbc16bf9b2fee26393
mutedalien <chel_c@mail.ru>
2020-04-15 21:52:34 +0300

    Урок 14 Полезные инструменты Обработка исключений

---
+++ less_14/or_true.py
@@ -0,0 +1,17 @@
+# добавление элемента в список
+
+# классический способ
+def add_to_list(input_list=None):
+    if input_list is None:
+        input_list = []
+    input_list.append(2)
+    return input_list
+
+result = add_to_list([0, 1])
+print(result)
+result = add_to_list()
+print(result)
+
+# через or
+def add_to_list(input_list=None):
+    # используем свойство or вместо условия
